813d4e3e73db95e84947326c4f2bcb5235d09091
Sydney McGinnis <77169150+pepperdog80@users.noreply.github.com>
2021-01-18 19:28:52 -0500

    Adding to main
    Added a loop to detect env variables in main. Added a function to print the env variable that the user types in. Added a function to parse the env variable path and deliminate based on colons. 
    
    -Sydney

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 void parser();
 
@@ -14,6 +16,8 @@ typedef struct
 void stringCompare(char *input);
 void exitFunction();
 void cdFunction();
+void findEnv(const char *input);	
+void findPath(const char *copyInput);
 
 char *get_input(void);
 tokenlist *get_tokens(char *input);
@@ -46,6 +50,18 @@ void parser(void)
 		for (int i = 0; i < tokens->size; i++) {
 			printf("token %d: (%s)\n", i, tokens->items[i]);
 		}
+		
+		for(int i = 0; i < tokens->size; i++)
+		{
+			if(strchr("$", *tokens->items[i]))
+			{
+				//if the token begins with a $ then it is an
+				//enviromental variable and needs to jump to 
+				//finEnv function
+				findEnv(tokens->items[i]);
+			}
+		}
+
 		
 		//stringCompare(input);
 		if(strcmp(tokens->items[0], "(echo)"))
@@ -154,6 +170,45 @@ bool containEnv(char *token)
 		token++;
 	}
 	return false;
+}
+
+void findEnv(const char *input)
+{
+	//prints the expansion of the env variable
+	//if the env variable is PATH then it jumps to the 
+	//findPath function
+	
+	const char *copyInput = input;
+	copyInput++;
+	
+	if(strcmp(copyInput, "PATH") == 0)
+	{
+		findPath(copyInput);
+	}
+	
+	else
+	printf("%s", getenv(copyInput));
+}
+void findPath(const char *copyInput)
+{
+	char *path = getenv(copyInput);
+	//parsing the enviromental varaible path
+	
+	for(int i = 0; i < strlen(path); i++)
+	{
+		if(path[i] == ':')
+		{
+			printf("%c", '\n');
+			//a colon deliminates one path from another
+			//if one is detected a new line will be 
+			//printed and the colon is ignored
+		}
+		else
+		{
+			printf("%c", path[i]);
+			//prints the paths character by character
+		}
+	}
 }
 
 /*
